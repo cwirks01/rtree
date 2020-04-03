fdb0c2abe4ca9af435d72f9cc19c3d679e9b3dab
Charles.Wirks <cwirks01@gmail.com>
2020-04-03 11:34:23 -0400

    Created code to test out rtree in a simulated env of unknown actors in unknown locations.

---
+++ rtree_sim.py
@@ -0,0 +1,29 @@
+import rtree as rt
+import os
+import pandas as pd
+import numpy as np
+
+ROOT = os.getcwd()
+randomSeed = [1] # Need random seed to perform test
+
+
+def _build_rtree(locations):
+    """ build environment of scenario using the bounded area in generate_actors """
+
+    return
+
+def generate_actors(num_of_actors,bounded_area):
+
+    return
+
+def actors_in_collect(actors):
+
+    return
+
+def build_tgt_decl(tgt_loc):
+
+    return
+
+if __name__=='__main__':
+
+# Run script here
