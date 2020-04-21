6ba2179565689721f74952e2690a734c681f7674
chase <cwirks01@gmail.com>
2020-04-21 15:33:14 -0400

    *** rtree_sim.py *** Corrected minor coding errors Replaced pd.DataFrame with lists Removed non-essential calls and replaced with R-tree index Removed tuple call Replaced deprecated time.clock() with .perf_counter() *** Plot_Scenario.py *** Included actors caught and total targets used for legend reference Changed X&Y to Longitude and Latitude reference Inserted annotate box to display total actors and targets

--- Plot_Scenario.py
+++ Plot_Scenario.py
@@ -9,6 +9,9 @@ filePath = os.path.abspath(os.path.join(ROOT, ".."))
 actor_location = pd.read_csv(os.path.join(filePath, "Desktop\\Collections.csv"))
 tgt_location = pd.read_csv(os.path.join(filePath, "Desktop\\Target_deck.csv"))
 
+totalActorsCaught = len(actor_location.iloc[:,0].drop_duplicates())
+totalTargets = len(tgt_location.iloc[:,0].drop_duplicates())
+
 tgt_location['height'] = tgt_location['top'] - tgt_location['bottom']
 tgt_location['width'] = tgt_location['right'] - tgt_location['left']
 tgt_location = pd.DataFrame(tgt_location)
@@ -24,6 +27,9 @@ for index, target in tgt_location.iterrows():
 
 ax.scatter(actor_location['left'], actor_location['bottom'], s=1, color='black')
 plt.title('Target Deck with Caught Actors')
-plt.xlabel('x')
-plt.ylabel('y')
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+plt.annotate("%s Targets Caught \n%s Targets in deck" % (totalActorsCaught, totalTargets), xy=(0.8, 1.01),
+             xycoords='axes fraction', fontsize=10)
+# plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
 plt.show()
--- rtree_sim.py
+++ rtree_sim.py
@@ -7,13 +7,14 @@ from rtree.index import Rtree
 import random
 import time
 
-randomSeed = 0  # Need random seed to perform test
-num_targets = 100
-num_actors = 1000
+RANDOMSEED = 0  # Need random seed to perform test
+NUM_TARGETS = 1000
+NUM_ACTORS = 10000
 
 ROOT = os.getcwd()
 outputFile = os.path.abspath(os.path.join(ROOT, ".."))
 
+#  Creating a file that will keep track of the statistics of the run
 timeEvalFile_path = os.path.join(outputFile, 'Desktop\\rtree_Stats.csv')
 if os.path.isfile(timeEvalFile_path):
     timeEvalFile = pd.read_csv(timeEvalFile_path)
@@ -22,18 +23,18 @@ else:
                                              'TimeToProcessScenario (s)'], index=[0])
 
 
-def _build_rtree(locations):
+def _build_rtree(locations, rtreeIdx):
     """ build environment of scenario using the bounded area in generate_actors """
     # Will initiate with the largest bounded area
     for location in locations['coord']:
         if location[1].startswith("Actor"):
-            idx.insert(4444, location[0], location[1])
+            rtreeIdx.insert(4444, location[0], location[1])
         else:  # id 5555 will be the box coordinates
-            idx.insert(5555, location[0], location[1])
+            rtreeIdx.insert(5555, location[0], location[1])
     return
 
 
-def generate_actors(num_of_actors):
+def generate_actors(num_of_actors=NUM_ACTORS):
     """
     Actors are randomly generated throughout a grid of a 1000 x 1000
     Inserting a point into a rtree left == right and bottom == top
@@ -43,38 +44,31 @@ def generate_actors(num_of_actors):
     """
     actors = dict()
     actors_loc = []
-    random.seed(randomSeed)
+    random.seed(RANDOMSEED)
     for i in range(num_of_actors):
         randomPoint_x, randomPoint_y = (random.randint(0, 1000), random.randint(0, 1000))
-        loc = tuple([(randomPoint_x, randomPoint_y, randomPoint_x, randomPoint_y), "Actor: " + str(i)])
-        if not (randomPoint_x < 0) or (randomPoint_y < 0):
-            actors_loc.append(loc)
-        else:
-            pass
+        loc = ((randomPoint_x, randomPoint_y, randomPoint_x, randomPoint_y), "Actor: " + str(i))
+        actors_loc.append(loc)
         actors['coord'] = actors_loc
     return actors
 
 
-def actors_in_collect(actors, targetDeck):
+def actors_in_collect(targetDeck, rtreeIdx):
     """
     This will be the method to find if the actors are in our target deck
     This will have a true and false statement where if true will return the actors id and coordinates as well as which
     box, or "target deck", they are in
     """
     all_collects = []
-    for i, target in targetDeck.iterrows():
-        hits = list(idx.intersection(target['Coordinates'], objects=True))
+    for i, target in enumerate(targetDeck):
+        hits = list(rtreeIdx.intersection(target[0], objects=True))
         collected = [(target[1], item.object, tuple(item.bbox)) for item in hits]
         all_collects.append(collected)
-    #
-    # for actor in actors['coord']:
-    #     hits = list(idx.intersection(actor[0], objects=True))
-    #     collected = [(actor[1], item.object, tuple(item.bbox)) for item in hits if item.id == 5555]
-    #     all_collects.append(collected)
+
     return all_collects
 
 
-def build_tgt_deck():
+def build_tgt_deck(num_targets=NUM_TARGETS):
     """
     build_tgt_deck generate randomly placed rectangles with varying sides on a grid of a 1000
     X and Y values less than zero will not be appended to the list of targets
@@ -82,17 +76,15 @@ def build_tgt_deck():
     """
     t_new = dict()
     tgt_deck = []
-    random.seed(randomSeed)
+    random.seed(RANDOMSEED)
     for i in range(num_targets):
-        randomPoint_x, randomPoint_y = (random.randint(0, 1000), random.randint(0, 1000))
+        randomPoint_x, randomPoint_y = (random.randint(10, 1000), random.randint(10, 1000))
         width, height = (random.randint(1, 5) * 2, random.randint(1, 5) * 2)
         x_min, x_max = (randomPoint_x - width, randomPoint_x + width)
         y_min, y_max = (randomPoint_y - height, randomPoint_y + height)
-        rect = tuple([(x_min, y_min, x_max, y_max), "Location:" + str(i)])
-        if not (x_min < 0) or (y_min < 0):
-            tgt_deck.append(rect)
-        else:
-            pass
+        rect = ((x_min, y_min, x_max, y_max), "Location:" + str(i))
+
+        tgt_deck.append(rect)
         t_new['coord'] = tgt_deck
 
     return t_new
@@ -102,21 +94,22 @@ def build_tgt_deck():
 if __name__ == '__main__':
     p = index.Property()
     idx = index.Index(properties=p)
-    actors_gen = generate_actors(num_actors)
-    actorScenario = pd.DataFrame(actors_gen['coord'], columns=['Coordinates', 'ActorsLocation'])
+    actors_gen = generate_actors()
+    actorScenario = pd.DataFrame(actors_gen['coord'], columns=['Coordinates', 'ActorsName'])
     tgt_location = build_tgt_deck()
-    locs = pd.DataFrame(tgt_location['coord'], columns=['Coordinates', 'Site'])
-    _build_rtree(actors_gen)
-    # _build_rtree(tgt_location) # Testing to evaluate targets separately from R-tree
+    locs = tgt_location['coord']
+    _build_rtree(actors_gen, idx)
+    # _build_rtree(tgt_location, idx) # Testing to evaluate targets separately from R-tree
 
     # Starting time after scenario is built
-    start = time.clock()
+    start = time.perf_counter()
 
-    actors_caught = actors_in_collect(actors_gen, locs)
-    actorsInTargets = len(
-        actors_caught)  # if multiple actors were caught in the same targeted box this will create another column
-    timeProcessActors = time.clock() - start
-    print("time to process %s actors and %s locations: %.03f s" % (num_actors, actorsInTargets, (timeProcessActors)))
+    actors_caught = actors_in_collect(locs, idx)
+
+    # if multiple actors were caught in the same targeted box this will create another column
+    actorsInTargets = len(actors_caught)
+    timeProcessActors = time.perf_counter() - start
+    print("time to process %s actors and %s locations: %.03f s" % (NUM_ACTORS, actorsInTargets, (timeProcessActors)))
     actors_caught = pd.DataFrame(actors_caught)
     colNames = []
     for i in range(len(actors_caught.columns)):
@@ -126,6 +119,7 @@ if __name__ == '__main__':
     actors_caught = actors_caught.stack().reset_index(drop=True)
     actors_caught = pd.DataFrame(actors_caught.tolist(), columns=['Location', 'Actors', 'Coordinates'])
 
+    locs = pd.DataFrame(tgt_location['coord'], columns=['Coordinates', 'TargetLocation'])
     new_col_list = ['left', 'bottom', 'right', 'top']
     for n, col in enumerate(new_col_list):
         actors_caught[col] = actors_caught['Coordinates'].apply(lambda location: location[n])
@@ -138,10 +132,10 @@ if __name__ == '__main__':
     actorScenario.to_csv(os.path.join(outputFile, "Desktop\\Actors_location.csv"), index=False)
 
     # Python time.clock is deprecated, but process_time and perf_counter does not send back ns
-    entireEventTime = time.clock() - start
+    entireEventTime = time.perf_counter() - start
     print("time to process whole event: %.03f s" % (entireEventTime))
     timeEvalFile_cols = timeEvalFile.columns
-    timeEvalFile = timeEvalFile.append({timeEvalFile_cols[0]: num_targets, timeEvalFile_cols[1]: num_actors,
+    timeEvalFile = timeEvalFile.append({timeEvalFile_cols[0]: NUM_TARGETS, timeEvalFile_cols[1]: NUM_ACTORS,
                                         timeEvalFile_cols[2]: timeProcessActors,
                                         timeEvalFile_cols[3]: entireEventTime}, True).dropna().reset_index(drop=True)
     timeEvalFile.to_csv(timeEvalFile_path, index=False)
